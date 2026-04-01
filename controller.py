e5de5768839ac9e21e317041f9804c0a8e885627
giulia1212 <s311742@studenti.polito.it>
2026-04-01 18:42:24 +0200

    Esercizio corretto. Laboratorio 4

--- controller.py
+++ controller.py
@@ -20,7 +20,7 @@ class SpellChecker:
                 parole = self._multiDic.searchWord(words, language)
                 for parola in parole:
                     if not parola.corretta:
-                        paroleErrate = paroleErrate + str(parola) + " - "
+                       paroleErrate = paroleErrate + str(parola) + " - "
                 t2 = time.time()
                 return paroleErrate, t2 - t1
 
@@ -44,6 +44,51 @@ class SpellChecker:
             case _:
                 return None
 
+    def handleSpellCheck(self, txtIn, language, modality):
+
+        if txtIn == "":
+            self._view._lvOut.controls.append(
+                ft.Text("Attenzione, il campo sentence non può essere vuoto.", color="red"))
+            self._view.update()         # ho cambiato da update_page() ad update() perchè in view c'è update()
+            return
+
+        if language is None:
+            self._view._lvOut.controls.append(
+                ft.Text("Attenzione, devi scegliere una lingua.", color="red"))
+            self._view.update()
+            return
+
+        if modality is None:
+            self._view._lvOut.controls.append(
+                ft.Text("Attenzione, devi scegliere una modalità di ricerca.", color="red"))
+            self._view.update()
+            return
+
+        paroleErrate, tempo = self.handleSentence(txtIn, language, modality)
+        self._view._lvOut.controls.append(ft.Text(f"Frase inserita: {txtIn}"))
+        self._view._lvOut.controls.append(
+            ft.Text(f"Parole errate:{paroleErrate} \nTempo impiegato: {tempo} secondi"))
+        self._view._txtSentence.value = ""
+        self._view.update()
+
+    def handleLanguageSelection(self, language):
+        if language is not None:
+            self._view._lvOut.controls.append(
+                ft.Text(f"Lingua selezionata: {language}", color="green"))
+        else:
+            self._view._lvOut.controls.append(
+                ft.Text("Errore nella selezione della lingua.", color="red"))
+        self._view.update()
+
+
+    def handleModalitySelection(self, modality):
+        if modality is not None:
+            self._view._lvOut.controls.append(
+                ft.Text(f"Modalità selezionata: {modality}", color="green"))
+        else:
+            self._view._lvOut.controls.append(
+                ft.Text("Errore nella selezione della modalità.", color="red"))
+        self._view.update()
 
     def printMenu(self):
         print("______________________________\n" +
@@ -62,3 +107,4 @@ def replaceChars(text):
     for c in chars:
         text = text.replace(c, "")
     return text
+
--- view.py
+++ view.py
@@ -14,6 +14,11 @@ class View(object):
         self.__theme_switch = None
 
         # define the UI elements and populate the page
+        self.ddLanguage = None      # menù a tendina lingua
+        self.ddModality = None      # modalità di ricerca
+        self._txtSentence = None    # frase
+        self._btnSpellCheck = None  # bottone
+        self._lvOut = None          # list view
 
     def add_content(self):
         """Function that creates and adds the visual elements to the page. It also updates
@@ -27,15 +32,49 @@ class View(object):
         )
 
         # Add your stuff here
+        # ROW1
+        # menù a tendina per scegliere la lingua
+        self.ddLanguage = ft.Dropdown(label="Select language",
+                                options=[ft.dropdown.Option("italian"),
+                                         ft.dropdown.Option("english"),
+                                         ft.dropdown.Option("spanish")],
+                                width=1200,
+                                on_change=lambda e: self.__controller.handleLanguageSelection(e.control.value))
 
-        # self.page.add([])
+        row1 = ft.Row([self.ddLanguage])
+
+        #ROW2
+        # modalità di ricerca
+        self.ddModality = ft.Dropdown(label="Search Modality",
+                                      options=[ft.dropdown.Option("Default"),
+                                               ft.dropdown.Option("Linear"),
+                                               ft.dropdown.Option("Dichotomic")],
+                                      width=300,
+                                      on_change=lambda e: self.__controller.handleModalitySelection(e.control.value))
+
+        self._txtSentence = ft.TextField(label="Add your sentence here", width=700)
+        self._btnSpellCheck = ft.ElevatedButton(text="Spell Check",
+                                                    on_click= lambda e: self.__controller.handleSpellCheck(
+                                                        self._txtSentence.value,
+                                                        self.ddLanguage.value,
+                                                        self.ddModality.value),
+                                                    width=200)
+
+        row2 = ft.Row([self.ddModality, self._txtSentence, self._btnSpellCheck])
+
+        # List view
+        self._lvOut = ft.ListView(expand=True)
+
+        self.page.add(row1, row2, self._lvOut)
 
         self.page.update()
 
     def update(self):
         self.page.update()
+
     def setController(self, controller):
         self.__controller = controller
+
     def theme_changed(self, e):
         """Function that changes the color theme of the app, when the corresponding
         switch is triggered"""
